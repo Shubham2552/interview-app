f868313ef25a04f750ac41cd4837ddf66f6cad08
Shubham Bhati <htcc58002900@gmail.com>
2025-06-10 20:42:06 +0530

    [interview-app]: fix cors

--- index.js
+++ index.js
@@ -34,7 +34,15 @@ app.use(limiter);
 // Request parsing and basic middleware
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json({ limit: '10mb' }));
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://verbalpilot.com',
+    'https://www.verbalpilot.com',
+    'https://pre-release.verbalpilot.com'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 app.use(requestIp.mw());
 
 // Request logging
